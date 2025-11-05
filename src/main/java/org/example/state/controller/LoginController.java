2f465a519af05606da12c8ab3c83799ee0ca9237
kimjava911 <aibackend4th@gmail.com>
2025-11-05 11:01:00 +0900

    step01

--- src/main/java/org/example/state/controller/LoginController.java
+++ src/main/java/org/example/state/controller/LoginController.java
@@ -7,13 +7,38 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet("/")
 public class LoginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 서블릿을 통해서 현재 GET 요청 처리
+        System.out.println(req.getMethod()); // 어떠한 HTTP 메서드(verb, 동사)로 입력했는지
+        // req(request) <- 요청 객체
+        // Spring MVC <- 요청 객체 중 원하는 부분만 따로 떼서 받게 됨
+        System.out.println(req.getParameter("msg")); // key(msg) -> input. // !
+        // form vs RESTful로 받느냐...
+        // HTTP 요청 -> header(meta 데이터)
+        Enumeration<String> headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            System.out.println(headerNames.nextElement());
+        }
+        // !
+        req.setAttribute("key", "value"); // servlet/jsp에서 이후에 전달하기 원하는 데이터 (<- model 쪽에서 뽑아오는 데이터나 중간 메시지...)
+        req.setAttribute("msg", req.getParameter("msg") + "라고 입력하셨습니다");
         // main -> java, webapp, resources -> 최상단으로 각각 인식됨
-        req.getRequestDispatcher("WEB-INF/views/login.jsp").forward(req, resp);
+        req.getRequestDispatcher("WEB-INF/views/login.jsp").forward(req, resp); // !
         // 외부의 URL은 그대로인 상태에서 뒤에 리소스만 연결 (servlet, jsp)
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 서블릿을 통해서 현재 POST 요청 처리
+        System.out.println(req.getMethod()); // 어떠한 HTTP 메서드(verb, 동사)로 입력했는지
+        System.out.println(req.getParameter("msg"));
+        req.setAttribute("key", "value");
+        req.setAttribute("msg", req.getParameter("msg") + "라고 입력하셨습니다");
+        req.getRequestDispatcher("WEB-INF/views/login.jsp").forward(req, resp); // !
     }
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -4,6 +4,30 @@
     <title>로그인 페이지</title>
 </head>
 <body>
-
+    <h3>Parameter</h3>
+    <p>
+        <%= request.getParameter("msg") %>
+    </p>
+    <h3>Attribute</h3>
+    <p>
+        <%= request.getAttribute("msg") %><br>
+        <%= request.getAttribute("key") %>
+    </p>
+    <%-- GET --%>
+    <%-- 현재 링크에 GET 요청 --%>
+    <form action="" method="get">
+        <input name="msg" placeholder="아무거나 입력해보세요">
+        <button>제출</button>
+    </form>
+    <%-- POST --%>
+    <%-- 현재 링크에 POST 요청 --%>
+    <%-- 주소창에 데이터 속성들이 노출되지 X --%>
+    <%-- REST API POST (application/json) vs Form POST (application/x-www-form-urlencoded) --%>
+    <%-- json: json body를 가지면서 + searchParam (주소창 데이터를 가질 수 있음) --%>
+    <%-- form: parameter만 가질 수 있음 (url 노출 되느냐 vs body로 들어가느냐) --%>
+    <form action="" method="post">
+        <input name="msg" placeholder="아무거나 입력해보세요">
+        <button>제출</button>
+    </form>
 </body>
 </html>
